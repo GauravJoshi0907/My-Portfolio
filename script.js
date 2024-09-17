e899e923dacd08eb5aab2b0df135448bf9d9b13e
GauravJoshi0907 <88457932+GauravJoshi0907@users.noreply.github.com>
2024-09-18 00:55:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
     // Add a click event listener to the list item
     resumeLink.addEventListener("click", function () {
       // Specify the URL of your resume
-      var resumeURL = "https://drive.google.com/file/d/1OmP5cV7bHpCyDY4nAC1xIJlwZOs_Yr_O/view?usp=sharing";
+      var resumeURL = "https://drive.google.com/file/d/1oQMB9dDBdhGp76rpPYjDIh95XTn-u0ZB/view?usp=sharing";
 
       // Open the URL in a new tab or window
       window.open(resumeURL, "_blank");
